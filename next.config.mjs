975088d649c9e5eebf0ecd5c0b93c3d84c76bfec
rameezqadir <160889398+rameezqadir@users.noreply.github.com>
2025-01-22 22:55:06 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// next.config.js
+module.exports = {
+    webpack: (config) => {
+      config.externals.push('@/data/rental-car-data.json');
+      return config;
+    },
+  };
